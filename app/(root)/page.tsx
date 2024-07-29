d9bf0a3bcde1151ab564da854dbe3ff6d3382491
NabilHasan09 <nabilhasan123456@gmail.com>
2024-07-28 21:19:31 -0400

    implenting a sidebar and starting mobile nav adjustments

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,7 +19,7 @@ const home = () => {
                     <TotalBalanceBox
                         accounts = {[]}
                         totalBanks = {1}
-                        totalCurrentBalance = {1250.35}
+                        totalCurrentBalance = {1250000.35}
                     />
 
                 </header>
--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,12 +1,11 @@
-import { formatAmount } from '@/lib/utils'
-import CountUp from 'react-countup'
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({accounts = [], totalBanks, totalCurrentBalance}: TotlaBalanceBoxProps) => {
   return (
     <section className="total-balance">
         <div className ="total-balance-chart">
-            {/* DoughnutChat */}
+            <DoughnutChart accounts={accounts} />
 
         </div>
 
@@ -20,10 +19,10 @@ const TotalBalanceBox = ({accounts = [], totalBanks, totalCurrentBalance}: Totla
                     Total Current Balance
                 </p>
 
-                <p className="total-balance-amount flex-center gap-2">
+                <div className="total-balance-amount flex-center gap-2">
                     <AnimatedCounter amount = {totalCurrentBalance}/>
 
-                </p>
+                </div>
             </div>
 
         </div>
